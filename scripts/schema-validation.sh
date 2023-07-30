08f62247a5590c1b90061bf785d274fbc00e67a2
Orun Bhuiyan <orun@agencyundone.com>
2023-07-29 23:56:29 -0400

    feat: add ajv json checker

--- scripts/schema-validation.sh
+++ scripts/schema-validation.sh
@@ -2,5 +2,5 @@
 set -e
 
 for file in $(git diff --name-only --diff-filter=AM $GITHUB_BEFORE..$GITHUB_AFTER | grep "content/projects/.*\.json"); do
-  npx ajv-cli validate -s project-schema.json -d "$file" || exit 1
+  npx ajv-cli validate -s scripts/schema-constraints.json -d "$file" || exit 1
 done
