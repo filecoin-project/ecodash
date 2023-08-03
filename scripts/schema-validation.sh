13f33fc1021e82f7ae3efac1c4e477104b4d7c2c
Orun Bhuiyan <orun@agencyundone.com>
2023-08-03 00:04:24 -0400

    feat: new file kebab validation

--- scripts/schema-validation.sh
+++ scripts/schema-validation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+ICON_ERRORS=0
 SCHEMA_ERRORS=0
 TAXONOMY_ERRORS=0
-ICON_ERRORS=0
+FILE_NAME_ERRORS=0
 
 # Perform schema validation with AJV
 for file in $(git diff --name-only --diff-filter=AM HEAD^..HEAD | grep "content/projects/.*\.json"); do
@@ -28,5 +29,19 @@ node scripts/check-images.js || ICON_ERRORS=$((ICON_ERRORS+1))
 
 if [[ "$ICON_ERRORS" -ne "0" ]]; then
   echo "🚫 $ICON_ERRORS icon errors found"
+  exit 1
+fi
+
+# Check new file names for kebab-case
+for file in $(git diff --name-only --diff-filter=A HEAD^..HEAD | grep "content/projects/.*\.json"); do
+  base=$(basename "$file")
+  if ! [[ $base =~ ^[a-z0-9\-]+\.json$ ]]; then
+    echo "File name error: $base is not in kebab-case format"
+    FILE_NAME_ERRORS=$((FILE_NAME_ERRORS+1))
+  fi
+done
+
+if [[ "$FILE_NAME_ERRORS" -ne "0" ]]; then
+  echo "🚫 $FILE_NAME_ERRORS json file names must be in kebab case"
   exit 1
 fi
